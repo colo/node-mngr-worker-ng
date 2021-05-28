160a8141d229c333c50547405270c59e02d32992
Luciano Bueno <glamero@gmail.com>
2021-05-28 17:47:30 -0300

    Added last auth to email accounts

--- apps/educativa/size/get_size_emails.sh
+++ apps/educativa/size/get_size_emails.sh
@@ -95,7 +95,7 @@ for line in `cat ${QMAIL_DIR}/control/virtualdomains`; do
 
 					if [ -d "${account_dir}" ]; then
 						size=`du -sk $account_dir | gawk -F '\t' '{print $1}'`
-						last_auth=`$VUSERINFO "$account_dir@${vdom}" | grep 'last auth:' | sed 's|last auth:||g' | xargs` #xargs trims var
+						last_auth=`$VUSERINFO "$account_dir@${vdom}" | grep 'last auth:' | sed 's|last\ auth:||g' | xargs` #xargs trims var
 						echo "$vdom|${account}|${size}|${last_auth}" >> ${LOG_FILE}
 						save "$vdom|${account}|${size}|${last_auth}"
 					fi
